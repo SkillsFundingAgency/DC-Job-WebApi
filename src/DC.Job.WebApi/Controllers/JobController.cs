dfaf16af96529e7e39bcc42639e838b00b2142fd
Salman Baig <salman.sfa@outlook.com>
2018-05-22 12:03:42 +0100

    added logger and logging

--- src/DC.Job.WebApi/Controllers/JobController.cs
+++ src/DC.Job.WebApi/Controllers/JobController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using ESFA.DC.JobQueueManager.Interfaces;
 using ESFA.DC.JobQueueManager.Models.Enums;
+using ESFA.DC.Logging.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ESFA.DC.Job.WebApi.Controllers
@@ -11,75 +12,99 @@ namespace ESFA.DC.Job.WebApi.Controllers
     public class JobController : Controller
     {
         private readonly IJobQueueManager _jobQueueManager;
+        private readonly ILogger _logger;
 
-        public JobController(IJobQueueManager jobQueueManager)
+        public JobController(IJobQueueManager jobQueueManager, ILogger logger)
         {
             _jobQueueManager = jobQueueManager;
+            _logger = logger;
         }
 
         // GET: api/Job
         [HttpGet]
         public IActionResult Get()
         {
-            var britishZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            _logger.LogInfo($"Get request recieved for all jobs");
 
-            var jobsList = _jobQueueManager.GetAllJobs().ToList();
-            jobsList.ForEach(x =>
+            try
             {
-                x.DateTimeSubmittedUtc = TimeZoneInfo.ConvertTime(DateTime.SpecifyKind(x.DateTimeSubmittedUtc, DateTimeKind.Unspecified), TimeZoneInfo.Local, britishZone);
-                x.DateTimeUpdatedUtc = TimeZoneInfo.ConvertTime(DateTime.SpecifyKind(x.DateTimeUpdatedUtc.GetValueOrDefault(), DateTimeKind.Unspecified), TimeZoneInfo.Local, britishZone);
-            });
+                var britishZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
 
-            jobsList = jobsList.OrderByDescending(x =>
-            {
-                switch (x.Status)
+                var jobsList = _jobQueueManager.GetAllJobs().ToList();
+                jobsList.ForEach(x =>
                 {
-                    case JobStatus.Completed:
-                        return 10;
-                    case JobStatus.Failed:
-                        return 20;
-                    case JobStatus.FailedRetry:
-                        return 30;
-                    case JobStatus.Paused:
-                        return 40;
-                    case JobStatus.MovedForProcessing:
-                    case JobStatus.Processing:
-                        return 50;
-                    default:
-                        return 60;
-                }
-            }).ThenByDescending(x => x.Priority).ThenBy(x => x.JobId).ToList();
-            return Ok(jobsList.ToList());
+                    x.DateTimeSubmittedUtc = TimeZoneInfo.ConvertTime(
+                        DateTime.SpecifyKind(x.DateTimeSubmittedUtc, DateTimeKind.Unspecified), TimeZoneInfo.Local, britishZone);
+                    x.DateTimeUpdatedUtc = TimeZoneInfo.ConvertTime(
+                        DateTime.SpecifyKind(x.DateTimeUpdatedUtc.GetValueOrDefault(), DateTimeKind.Unspecified), TimeZoneInfo.Local, britishZone);
+                });
+
+                jobsList = jobsList.OrderByDescending(x =>
+                {
+                    switch (x.Status)
+                    {
+                        case JobStatus.Completed:
+                            return 10;
+                        case JobStatus.Failed:
+                            return 20;
+                        case JobStatus.FailedRetry:
+                            return 30;
+                        case JobStatus.Paused:
+                            return 40;
+                        case JobStatus.MovedForProcessing:
+                        case JobStatus.Processing:
+                            return 50;
+                        default:
+                            return 60;
+                    }
+                }).ThenByDescending(x => x.Priority).ThenBy(x => x.JobId).ToList();
+
+                _logger.LogInfo($"Returning jobs list with count : {jobsList.Count}");
+                return Ok(jobsList.ToList());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Get all jobs failed", ex);
+                return BadRequest();
+            }
         }
 
         // GET: api/Job
         [HttpGet("{jobId}")]
         public IActionResult Get(long jobId)
         {
+            _logger.LogInfo($"Request recieved to get the with Id : {jobId}");
+
             if (jobId == 0)
             {
+                _logger.LogWarning($"Request recieved with Job id 0");
                 return BadRequest();
             }
 
             var job = _jobQueueManager.GetJobById(jobId);
+            _logger.LogInfo($"Returning job successfully with job id :{job.JobId}");
             return Ok(job);
         }
 
         [HttpPost]
         public ActionResult Post([FromBody]JobQueueManager.Models.Job job)
         {
+            _logger.LogInfo("Post for job recieved for job : {@job} ", new[] { job });
             if (job == null)
             {
+                _logger.LogWarning($"Job Post request received with empty data");
                 return BadRequest();
             }
 
             if (!Enum.IsDefined(typeof(JobStatus), job.Status))
             {
+                _logger.LogWarning($"Job Post request received with bad status {job.Status}");
                 return BadRequest("Status is not a valid value");
             }
 
             if (!Enum.IsDefined(typeof(JobType), job.JobType))
             {
+                _logger.LogWarning($"Job Post request received with bad job type {job.JobType}");
                 return BadRequest("Job type is not a valid value");
             }
 
@@ -90,36 +115,47 @@ namespace ESFA.DC.Job.WebApi.Controllers
                     if (job.Status == JobStatus.Ready || job.Status == JobStatus.Paused ||
                         job.Status == JobStatus.FailedRetry)
                     {
+                        _logger.LogInfo($"Going to update job with job Id : {job.JobId}");
+
                         var result = _jobQueueManager.UpdateJob(job);
                         if (result)
                         {
+                            _logger.LogInfo($"Successfully updated job with job Id : {job.JobId}");
                             return Ok();
                         }
                         else
                         {
+                            _logger.LogWarning($"Update job failed for job Id : {job.JobId}");
                             return BadRequest();
                         }
                     }
                     else
                     {
+                        _logger.LogWarning($"Update job rejected because job status is not updateable for job Id : {job.JobId}, status : {job.Status}");
                         return BadRequest("Job with status of Ready,Paused or FailedRetry can only be updated.");
                     }
                 }
                 else
                 {
-                   job.JobId = _jobQueueManager.AddJob(job);
+                    _logger.LogInfo($"Create Job request received with object : {job} ");
+
+                    job.JobId = _jobQueueManager.AddJob(job);
                     if (job.JobId > 0)
                     {
+                        _logger.LogInfo($"Created job successfully with Id : {job.JobId} ");
                         return Ok(job.JobId);
                     }
                     else
                     {
+                        _logger.LogInfo("Create job failed for job : {@job} ", new[] { job });
                         return BadRequest();
                     }
                 }
             }
             catch (Exception ex)
             {
+                _logger.LogError("Post for job failed for job : {@job} ", ex, new[] { job });
+
                 return BadRequest();
             }
         }
@@ -127,6 +163,8 @@ namespace ESFA.DC.Job.WebApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
+            _logger.LogInfo($"Delete for job recieved for job id : {id} ");
+
             if (id == 0)
             {
                 return BadRequest();
@@ -139,6 +177,8 @@ namespace ESFA.DC.Job.WebApi.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError($"Delete for job failed for job : {id} ", ex);
+
                 return BadRequest();
             }
         }
--- src/DC.Job.WebApi/Controllers/ValidationErrorsController.cs
+++ src/DC.Job.WebApi/Controllers/ValidationErrorsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ESFA.DC.Logging.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ValidationErrorStub;
@@ -13,15 +14,19 @@ namespace ESFA.DC.Job.WebApi.Controllers
     public class ValidationErrorsController : ControllerBase
     {
         private readonly IValidationErrorsReportService _validationErrorsReportService;
+        private readonly ILogger _logger;
 
-        public ValidationErrorsController(IValidationErrorsReportService validationErrorsReportService)
+        public ValidationErrorsController(IValidationErrorsReportService validationErrorsReportService, ILogger logger)
         {
             _validationErrorsReportService = validationErrorsReportService;
+            _logger = logger;
         }
 
         [HttpGet("{ukprn}")]
         public async Task<IEnumerable<ValidationError>> Get(long ukprn)
         {
+            _logger.LogInfo($"Get request recieved for validation errors ukprn : {ukprn}");
+
             return await _validationErrorsReportService.GetValidationErrorsAsync(ukprn);
         }
     }
